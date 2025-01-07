d47d678f7d1ee1281fcbc894f58b2fdf81226c16
Pablo Bilbao <pablobilbao1605@gmail.com>
2025-01-08 00:36:59 +0100

    several adjustments

--- files/calibration.py
+++ files/calibration.py
@@ -19,9 +19,8 @@ def refine_corners(corners: List, imgs: List) -> List:
     imgs_gray = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in imgs]
     return [cv2.cornerSubPix(i, cor[1], (7, 7), (-1, -1), criteria) if cor[0] else [] for i, cor in zip(imgs_gray, corners_copy)]
 
-def draw_show_and_save_corners(imgs_copy: List, corners: List, corners_refined: List, output_dir: str) -> None:
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)  # Crea el directorio si no existe
+def draw_show_corners(imgs_copy: List, corners: List, corners_refined: List) -> None:
+    
     
     for i, (img, cor, found) in enumerate(zip(imgs_copy, corners_refined, [c[0] for c in corners])):
         if found:
@@ -31,10 +30,6 @@ def draw_show_and_save_corners(imgs_copy: List, corners: List, corners_refined: 
             cv2.imshow(f'Image {i}', img)
             cv2.waitKey(500)  # Muestra cada imagen durante 500 ms (ajustable)
 
-            # Guarda la imagen si no existe
-            # filename = os.path.join(os.getcwd(), f"foto_{i}.png")
-            if not os.path.exists("./data/foto_{i}.jpg"):
-                cv2.imwrite(f"./data/foto_{i}.jpg", img)
     
     cv2.destroyAllWindows()  # Cierra todas las ventanas de OpenCV
 
@@ -51,8 +46,7 @@ def main():
     corners = find_corners(imgs)
     corners_refined = refine_corners(corners,imgs)
     imgs_copy = copy.deepcopy(imgs)
-    output_dir = "/home/pi/FinalProjectComputerVision/data/output/"
-    draw_show_and_save_corners(imgs_copy, corners, corners_refined, output_dir)
+    draw_show_corners(imgs_copy, corners, corners_refined)
     
     chessboard_points = get_chessboard_points((7, 7), 30, 30)
     valid_corners = [cor[1] for cor in corners if cor[0]]
